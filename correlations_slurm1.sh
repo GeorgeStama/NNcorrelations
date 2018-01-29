4f4836e3f34e82a9ee113584d19a991c35c1b183
GeorgeStama <george.stamatescu@gmail.com>
2018-01-29 15:53:46 +1030

    cuda_default

--- correlations_slurm1.sh
+++ correlations_slurm1.sh
@@ -17,5 +17,5 @@ pyenv activate pytorch
 export DATA_DIR=/fast/users/a1195560/NNcorrelations
 export CHECKPOINT_DIR=/fast/users/a1195560/corrChkpts
 
-python EBPcudaSoudry.py 
+python cudaluca_3layers_801.py 
 -num_views=2 --use_context --teacher_forcing=1.0 --vgg_loss
