9cba6a174b6fe9067d5b9baade814a495e43e382
GeorgeStama <george.stamatescu@gmail.com>
2018-02-03 18:37:53 +1030

    cudabinary final

--- cuda_binary3layers.py
+++ cuda_binary3layers.py
@@ -10,6 +10,8 @@ from torch.autograd import Variable
 import math
 from torch.nn.parameter import Parameter
 import time
+from FilteredMNIST import FilteredMNIST
+
 #import matplotlib.pyplot as plt
 
 # Training settings
@@ -18,13 +20,13 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=2, metavar='N',
+parser.add_argument('--epochs', type=int, default=5, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.8, metavar='M',
                     help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=True,
+parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
@@ -34,6 +36,7 @@ args = parser.parse_args()
 
 
 class MVG_binaryNet(nn.Module):
+
     def __init__(self, H1, H2, dropprob, scale):
         super(MVG_binaryNet, self).__init__()
 
@@ -41,7 +44,7 @@ class MVG_binaryNet(nn.Module):
         self.drop_prob = dropprob
         self.hidden1 = H1
         self.hidden2 = H2
-        self.D_out = 10
+        self.D_out = 1
         self.scale = scale
         self.w0 = Parameter(torch.Tensor(28 * 28, self.hidden1))
         stdv = 1. / math.sqrt(self.w0.data.size(1))
@@ -73,10 +76,6 @@ class MVG_binaryNet(nn.Module):
         self.th3 = Parameter(torch.zeros(1,self.hidden2))
         self.th4 = Parameter(torch.zeros(1,self.hidden2))
         self.thlast = Parameter(torch.zeros(1, self.D_out))
-
-    def expected_loss(self, target, forward_result):
-        (a2, logprobs_out) = forward_result
-        return F.nll_loss(logprobs_out, target)
 
     def MVG_layer(self, xbar, xcov, m, th):
         # recieves neuron means and covariance, returns next layer means and covariances
@@ -142,14 +141,24 @@ class MVG_binaryNet(nn.Module):
         x4bar, xcov_4 = self.MVG_layer(F.dropout(x3bar, p=self.drop_prob, training=self.training), xcov_2, m3, self.th3)
         #x5bar, xcov_5 = self.MVG_layer(F.dropout(x4bar, p=self.drop_prob, training=self.training), xcov_4, m4, self.th4)
 
-        hlastbar = x4bar.mm(mlast) + self.thlast.repeat(x1bar.size()[0], 1)
+        H, H2 = mlast.size()
+        sigmalast = torch.t(mlast)[None, :, :].repeat(M, 1, 1).bmm(xcov_4.clone().bmm(mlast.repeat(M, 1, 1))) + torch.diag(
+            torch.sum(1 - mlast ** 2, 0)).repeat(M, 1, 1)
+        tem = sigmalast.clone().resize(M, H2 * H2)
+        diagsiglast = tem[:, ::(H2 + 1)]
 
-        logprobs_out = F.log_softmax(hlastbar)
-        val, ind = torch.max(hlastbar, 1)
-        tem = y.type(dtype) - ind.type(dtype)[:, None]
-        fraction_correct = (M_double - torch.sum((tem != 0)).type(dtype)) / M_double
-        expected_loss = self.expected_loss(target, (hlastbar, logprobs_out))
-        return ((hlastbar, logprobs_out, xcov_3)), expected_loss, fraction_correct
+        hlastbar = x4bar.mm(mlast) + self.thlast.repeat(x1bar.size()[0], 1)
+        hlast = sq2pi*hlastbar/torch.sqrt(diagsiglast)
+
+        loss_binary = nn.BCELoss()
+        m = nn.Sigmoid()
+        expected_loss = loss_binary(torch.squeeze(m(hlast)), torch.squeeze(y[:,0]).type(dtype))
+        logprobs_out = torch.log(m(hlast))
+        pred = (torch.sigmoid(hlast) > 0.5).type(dtype)
+        a = torch.abs((pred - y.type(dtype)))
+        fraction_correct = (M_double - torch.sum(a)) / M_double
+
+        return ((hlastbar,logprobs_out, xcov_4)), expected_loss, fraction_correct
 
 class MVG_binaryNetRelaxed(nn.Module):
     def __init__(self, H1, H2, dropprob, scale):
@@ -161,7 +170,7 @@ class MVG_binaryNetRelaxed(nn.Module):
         self.drop_prob = dropprob
         self.hidden1 = H1
         self.hidden2 = H2
-        self.D_out = 10
+        self.D_out = 11
         self.scale = scale
         self.w0 = Parameter(torch.Tensor(28 * 28, self.hidden1))
         stdv = 1. / math.sqrt(self.w0.data.size(1))
@@ -274,7 +283,7 @@ class EBP_binaryNetRelaxed(nn.Module):
         self.sq2pi = 0.797884560
         self.beta = 1.0
         self.hidden = H
-        self.D_out = 10
+        self.D_out = 1
         self.scale = scale
 
         self.w0 = Parameter(torch.Tensor(28 * 28,  self.hidden))
@@ -384,7 +393,7 @@ class EBP_binaryNet(nn.Module):
         self.sq2pi = 0.797884560
         self.samp = 20
         self.hidden = H1
-        self.D_out = 10
+        self.D_out = 1
         self.scale = scale
         self.w0 = Parameter(torch.Tensor(28 * 28,  self.hidden))
         stdv = 1. / math.sqrt(self.w0.data.size(1))
@@ -433,14 +442,8 @@ class EBP_binaryNet(nn.Module):
         # x covariance across layer 2
         xc2 = (1 - xbar_next ** 2)
         xcov_next = Variable(torch.eye(H).cuda())[None, :, :] * (1 - xbar_next[:, None, :] ** 2)
-        #xcov_next = Variable(torch.eye(H))[None, :, :] * (1 - xbar_next[:, None, :] ** 2)
 
         return xbar_next, xcov_next
-
-    def expected_loss(self, target, forward_result):
-        (a2, logprobs_out) = forward_result
-        return F.nll_loss(logprobs_out, target)
-
 
     def forward(self, x, target):
         m0 = 2 * F.sigmoid(self.w0) - 1
@@ -468,7 +471,7 @@ class EBP_binaryNet(nn.Module):
         #bn = nn.BatchNorm1d(h1.size()[1], affine=False)
         x1bar = torch.tanh(h1)  #
         x1bar_d0 = F.dropout(x1bar, p=self.drop_prob, training=self.training)
-        ey =  Variable(torch.eye(H1))#.cuda())
+        ey =  Variable(torch.eye(H1).cuda())
         xcov_1 = ey[None, :, :] * ( 1 - x1bar_d0[:, None, :] ** 2)  # diagonal of the layer covariance - ie. the var of neuron i
 
         '''NEW LAYER FUNCTION'''
@@ -484,15 +487,18 @@ class EBP_binaryNet(nn.Module):
         diagsiglast = tem[:, ::(H2 + 1)]
 
         hlastbar = (x4bar.mm(mlast) + self.thlast.repeat(x1bar.size()[0], 1))
-        #hlast = sq2pi*hlastbar/torch.sqrt(diagsiglast)
+        hlast = sq2pi*hlastbar/torch.sqrt(diagsiglast)
 
-        logprobs_out = F.log_softmax(hlastbar)
-        val, ind = torch.max(hlastbar, 1)
-        tem = y.type(dtype) - ind.type(dtype)[:, None]
-        fraction_correct = (M_double - torch.sum((tem != 0)).type(dtype)) / M_double
-        expected_loss =  self.expected_loss(target, (hlastbar, logprobs_out))
 
-        return ((hlastbar, logprobs_out,xcov_4)), expected_loss, fraction_correct
+        loss_binary = nn.BCELoss()
+        m = nn.Sigmoid()
+        expected_loss = loss_binary(torch.squeeze(m(hlast)), torch.squeeze(y[:,0]).type(dtype))
+        logprobs_out = torch.log(m(hlast))
+        pred = (torch.sigmoid(hlast) > 0.5).type(dtype)
+        a = torch.abs((pred - y.type(dtype)))
+        fraction_correct = (M_double - torch.sum(a)) / M_double
+
+        return ((hlastbar,logprobs_out, xcov_4)), expected_loss, fraction_correct
 
 
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -502,13 +508,15 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
 train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data_mnist', train=True, download=True,
-                   transform=transforms.ToTensor()),
+    FilteredMNIST('data_mnist', train=True, download=True,
+                   transform=transforms.ToTensor(),classes=[2,3]),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data_mnist', train=False, transform=transforms.ToTensor()),
+    FilteredMNIST('data_mnist', train=False, transform=transforms.ToTensor(),classes=[2,3]),
     batch_size=args.batch_size, shuffle=True, **kwargs)
+
 
 
 def train(epoch, model):
@@ -519,13 +527,20 @@ def train(epoch, model):
     train_loss_loop = torch.zeros(938)
     for batch_idx, (data, target) in enumerate(train_loader):
         start = time.time()
+
         if args.cuda:
             data, target = data.cuda(), target.cuda()
+        maxtarget = torch.max(target)
+        target = (target == maxtarget).type(torch.DoubleTensor)
+
         data, target = Variable(data), Variable(target)
+        x = data.view(-1, 28 * 28)
+        y = target[:, None]
+        M = x.size()[0]
 
         optimizer.zero_grad()
-        ((a2, logprobs_out, xcov_3)), loss, frac_corr = model(data, target)
-
+        ((a2,logprobs_out, xcov_3)), loss, frac_corr = model(data, target)
+        #print('hbar', torch.sigmoid(a2), target)
         loss.backward()
         train_loss += loss.data[0]
         frac_correct_sum += frac_corr.data[0]
@@ -534,6 +549,7 @@ def train(epoch, model):
 
         optimizer.step()
         if batch_idx % args.log_interval == 0:
+            loss_binary = nn.BCELoss()
             pos = torch.sum((xcov_3>0))
             neg = torch.sum((xcov_3<0))
             #print(xcov_3[0,:,:])#,xcov_3.size(), pos,neg)
@@ -555,21 +571,24 @@ def test(epoch, model):
     for data, target in test_loader:
         if args.cuda:
             data, target = data.cuda(), target.cuda()
+        maxtarget = torch.max(target)
+        target = (target == maxtarget).type(torch.DoubleTensor)
+
         data, target = Variable(data), Variable(target)
         ((a2, logprobs_out,xcov_3)), loss, frac_corr = model(data, target)
         test_loss += loss.data[0]
         frac_correct_sum += frac_corr.data[0]
         count += 1
         pred = logprobs_out.data.max(1)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data).cpu().sum()
+        #correct += pred.eq(target.data).cpu().sum()
     test_loss = test_loss
     test_loss /= len(test_loader)  # loss function already averages over batch size
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    return test_loss / len(test_loader.dataset), 100. * correct / len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: ({:.0f}%)\n'.format(
+        test_loss,  100. * frac_correct_sum / count))
+        #100. * correct / len(test_loader.dataset)))
+    return test_loss / len(test_loader.dataset),100. * frac_correct_sum / count
 
-Hs = np.array([[30,30]])
+Hs = np.array([[50,50]])
 scale_arr = np.array([[0.1]])
 LR = 1e-2
 drop_prb = 0.
@@ -587,17 +606,31 @@ testcorr_avg_MVG = torch.zeros(args.epochs,len(Hs),len(scale_arr))
 traincorr_avg_MVG = torch.zeros(args.epochs,len(Hs),len(scale_arr))
 
 
+#print('Full covariance')
+for dr in range(len(scale_arr)):
+    scale = scale_arr[dr][0]
+    for l in range(len(Hs)):
+        H1, H2 = Hs[l]
+        #model = MVG_binaryNet(H1, H2)
+        modelbin_mvg = MVG_binaryNet(H1, H2,drop_prb,scale)
+
+        optimizer = optim.SGD(modelbin_mvg.parameters(), lr=LR)
+
+        for epoch in range(1, args.epochs + 1):
+            traincorr_avg_MVG[epoch - 1, l, dr] = train(epoch,modelbin_mvg)
+            f, testcorr_avg_MVG[epoch - 1,l,dr] = test(epoch,modelbin_mvg)
+
+
+#print('Diagonal covariance')
+
 for dr in range(len(scale_arr)):
     scale = scale_arr[dr][0]
     for l in range(len(Hs)):
         H1, H2 = Hs[l]
         #model = MVG_binaryNet(H1, H2)
         modelbin_ebp = EBP_binaryNet(H1,drop_prb,scale)
-        #modelbin_ebp.load_state_dict(torch.load('pickledpy.py'))
 
-        #modelbin_ebp = EBP_binaryNet(H1, drop_prb, scale)
-
-        optimizer = optim.Adam(modelbin_ebp.parameters(), lr=LR)
+        optimizer = optim.SGD(modelbin_ebp.parameters(), lr=LR)
 
         for epoch in range(1, args.epochs + 1):
             traincorr_avg_EBP[epoch - 1, l, dr] = train(epoch,modelbin_ebp)
@@ -605,6 +638,9 @@ for dr in range(len(scale_arr)):
 
 
 # ... after training, save your model
-torch.save(modelbin_ebp.state_dict(), 'lucasave_1.py')
+torch.save(modelbin_ebp.state_dict(), 'binaryClssify.py')
 # .. to load your previously training model:
-#modelbin_ebp.load_state_dict(torch.load('pickledpy.py'))
+np.save('trnEBP',torch.squeeze(traincorr_avg_EBP).numpy())
+np.save('trnMVG',torch.squeeze(traincorr_avg_MVG).numpy())
+np.save('testEBP',torch.squeeze(traincorr_avg_MVG).numpy())
+np.save('testMVG',torch.squeeze(traincorr_avg_MVG).numpy())
---
+++ twoClass_mnist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:1
+
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=george.stamatescu@student.adelaide.edu.au
+
+source $HOME/.bashrc
+
+pyenv activate pytorch
+
+export DATA_DIR=/fast/users/a1195560/NNcorrelations
+export CHECKPOINT_DIR=/fast/users/a1195560/corrChkpts
+
+python cuda_binary3layers.py 
+-num_views=2 --use_context --teacher_forcing=1.0 --vgg_loss
