2bae68ae9dd45e1837d35c2b09f63a5df4eba92d
George Stefan Stamatescu <a1195560@l03.phoenix.adelaide.edu.au>
2018-02-05 18:23:54 +1030

    dropout SGD

--- cuda_binary3layers.py
+++ cuda_binary3layers.py
@@ -20,7 +20,7 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=150, metavar='N',
+parser.add_argument('--epochs', type=int, default=250, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                     help='learning rate (default: 0.01)')
@@ -586,10 +586,10 @@ def test(epoch, model):
         #100. * correct / len(test_loader.dataset)))
     return test_loss / len(test_loader.dataset),100. * frac_correct_sum / count
 
-Hs = np.array([[11,11]])
+Hs = np.array([[101,101]])
 scale_arr = np.array([[0.1]])
-LR = 1e-4
-drop_prb = 0.
+LR = 5e-3
+drop_prb = 0.4
 
 testcorr_avg_EBPrelaxed = torch.zeros(args.epochs,len(Hs),len(scale_arr))
 traincorr_avg_EBPrelaxed = torch.zeros(args.epochs,len(Hs),len(scale_arr))
@@ -641,7 +641,7 @@ for dr in range(len(scale_arr)):
 torch.save(modelbin_ebp.state_dict(), 'binaryClssifyEBP.py')
 torch.save(modelbin_ebp.state_dict(), 'binaryClssifyMVG.py')
 # .. to load your previously training model:
-np.save('trnEBP',torch.squeeze(traincorr_avg_EBP).numpy())
-np.save('trnMVG',torch.squeeze(traincorr_avg_MVG).numpy())
-np.save('testEBP',torch.squeeze(testcorr_avg_EBP).numpy())
-np.save('testMVG',torch.squeeze(testcorr_avg_MVG).numpy())
+np.save('trnEBPs',torch.squeeze(traincorr_avg_EBP).numpy())
+np.save('trnMVGs',torch.squeeze(traincorr_avg_MVG).numpy())
+np.save('testEBPs',torch.squeeze(testcorr_avg_EBP).numpy())
+np.save('testMVGs',torch.squeeze(testcorr_avg_MVG).numpy())
